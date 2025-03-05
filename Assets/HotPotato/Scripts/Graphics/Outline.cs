acf5a6aa419e38684fcdd8bb432df20da1c4e4c7
Ferchus <ferchusgames@protonmail.com>
2025-03-05 11:01:04 -0600

    Synchronizing outlines

--- Assets/HotPotato/Scripts/Graphics/Outline.cs
+++ Assets/HotPotato/Scripts/Graphics/Outline.cs
@@ -1,27 +1,17 @@
+using FishNet.Object;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using FishNet.Connection;
 
-public class Outline
-    : MonoBehaviour,
-        IPointerEnterHandler,
-        IPointerExitHandler
+public class Outline : NetworkBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField, DrawWithUnity]
     private RenderingLayerMask outlineLayer;
 
-    [SerializeField]
-    private Activate activate = Activate.OnHover;
-
     private Renderer[] renderers;
     private uint originalLayer;
     private bool isOutlineActive;
-
-    private enum Activate
-    {
-        OnHover,
-        OnClick
-    }
 
     private void Start()
     {
@@ -33,19 +23,32 @@ public class Outline
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (activate != Activate.OnHover)
-            return;
-        SetOutline(true);
+        ApplyOutline(true);
+        RequestOutlineChange(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (activate != Activate.OnHover)
-            return;
-        SetOutline(false);
+        ApplyOutline(false);
+        RequestOutlineChange(false);
     }
-    
-    private void SetOutline(bool enable)
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestOutlineChange(bool enable, NetworkConnection requestingClient = null)
+    {
+        RpcUpdateOutline(enable, requestingClient);
+    }
+
+    [ObserversRpc]
+    private void RpcUpdateOutline(bool enable, NetworkConnection requestingClient)
+    {
+        if (requestingClient == LocalConnection)
+            return;
+
+        ApplyOutline(enable);
+    }
+
+    private void ApplyOutline(bool enable)
     {
         foreach (var rend in renderers)
         {
