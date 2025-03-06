0ab0683f394f9a7133bdcbc5ec338779aafc1285
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2025-03-06 13:27:01 -0600

    Select a set of random modules and mark them as traps (#67)
    Closes #56 
    
    When modules are spawned, the BombSpawner selects a random list of
    modules and sets them as traps. We can control how many traps to spawn.
    
    https://github.com/user-attachments/assets/a9873008-aebe-423f-a4de-9f16d4eb3ed8

--- Assets/HotPotato/Scripts/Bomb/BombSpawner.cs
+++ Assets/HotPotato/Scripts/Bomb/BombSpawner.cs
@@ -1,4 +1,5 @@
-﻿using FishNet.Object;
+﻿using System.Collections.Generic;
+using FishNet.Object;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -6,17 +7,25 @@ namespace HotPotato.Bomb
 {
     public class BombSpawner : NetworkBehaviour
     {
-        [BoxGroup("Bomb Modules"), Tooltip("List of bomb module prefabs to spawn.")]
-        [Required, SerializeField] private BombModule[] _bombModulePrefabs;
+        [BoxGroup("Bomb Settings"), Tooltip("Number of modules to mark as traps."), MinValue(3)]
+        [SerializeField] private int _trapAmount = 3;
+        
+        [BoxGroup("Bomb Modules"), Tooltip("List of bomb module prefabs to spawn."), Required, AssetsOnly]
+        [SerializeField] private BombModule[] _bombModulePrefabs;
+
+        [BoxGroup("Bomb Modules"), Tooltip("GameObject the modules will spawn in."), Required, SceneObjectsOnly]
+        [SerializeField] private Transform _bombModuleParent;
         
         [BoxGroup("Grid Settings"), Tooltip("Defines the size of the module grid (between 2 and 10).")]
-        [Range(2, 10), SerializeField] private int _gridSize = 5;
+        [SerializeField, Range(2, 10)] private int _gridSize = 5;
 
         [BoxGroup("Grid Settings"), Tooltip("Determines the scale of each module.")]
         [SerializeField] private float _unitaryScale = 10f;
 
         [BoxGroup("Grid Settings"), Tooltip("Determines the spacing between modules.")]
         [SerializeField] private float _caseSize = 0.5f;
+        
+        private HashSet<int> _trapIndexes;
         
         public override void OnStartServer()
         {
@@ -26,21 +35,36 @@ namespace HotPotato.Bomb
         [Server]
         private void SpawnModuleGrid()
         {
-           for (int column = 0; column < _gridSize; column++)
-           {
-               for (int row = 0; row < _gridSize; row++)
+            ClampTrapAmount();
+            InitializeTrapIndexes();
+            
+            var currentModule = 0;
+            
+            for (var column = 0; column < _gridSize; column++) 
+            {
+               for (var row = 0; row < _gridSize; row++)
                {
                    GameObject bombModule = Instantiate(
                        GetRandomModule(),
                        GetModulePosition(column, row),
-                       Quaternion.identity
+                       Quaternion.identity,
+                       _bombModuleParent
                    );
                    
                    bombModule.transform.localScale = new Vector3(GetModuleScale(), 1, GetModuleScale());
+                   bombModule.name = $"Bomb Module {column} {row}";
                    base.Spawn(bombModule);
-                   bombModule.GetComponent<BombModule>().SetSettings(GetRandomSetting());
+                   
+                   bombModule.GetComponent<BombModule>().SetSettings(GetSettings(currentModule));
+                   
+                   currentModule++;
                }
-           }
+            }
+        }
+
+        private void ClampTrapAmount()
+        { 
+            _trapAmount = Mathf.Min(_trapAmount, GetModuleCount());
         }
 
         private GameObject GetRandomModule()
@@ -50,7 +74,7 @@ namespace HotPotato.Bomb
 
         private Vector3 GetModulePosition(int row, int column)
         {
-            Vector3 position = new Vector3(
+            var position = new Vector3(
                 GetFirstPositionOffset() + row * GetOffsetBetweenModules(),
                 0,
                 GetFirstPositionOffset() + column * GetOffsetBetweenModules()
@@ -72,20 +96,35 @@ namespace HotPotato.Bomb
         {
             return _unitaryScale / _gridSize;
         }
+        
+        private int GetModuleCount()
+        {
+            return _gridSize * _gridSize;
+        }
 
-        private BombModuleSettings GetRandomSetting()
+        private BombModuleSettings GetSettings(int currentModuleIndex)
         {
             return new BombModuleSettings
             {
                 ColorIndex = GetRandomSettingIndex(),
                 NumberIndex = GetRandomSettingIndex(),
-                LetterIndex = GetRandomSettingIndex()
+                LetterIndex = GetRandomSettingIndex(),
+                IsTrap = _trapIndexes.Contains(currentModuleIndex)
             };
         }
 
         private int GetRandomSettingIndex()
         {
             return Random.Range(0, 5);
+        }
+        
+        private void InitializeTrapIndexes()
+        {
+            _trapIndexes = new HashSet<int>();
+            while (_trapIndexes.Count < _trapAmount)
+            {
+                _trapIndexes.Add(Random.Range(0, _gridSize * _gridSize));
+            }
         }
     }
 
@@ -94,5 +133,6 @@ namespace HotPotato.Bomb
         public int ColorIndex;
         public int NumberIndex;
         public int LetterIndex;
+        public bool IsTrap;
     }
 }
