1513b674e01af0cbb879b7b81c7534169d709cd4
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2025-03-27 18:55:19 -0600

    Created window for editor scene change buttons (#90)
    How to use it:
    
    ![image](https://github.com/user-attachments/assets/d462bd66-658f-4783-9e6a-c70c1d377a40)
    
    Window:
    
    ![image](https://github.com/user-attachments/assets/effb9fcd-059c-4208-9021-ed259ba104b5)

---
+++ Assets/Utilities/Odin/HelpfulButtons.cs
@@ -0,0 +1,42 @@
+﻿using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Utilities.Odin
+{
+    public class HelpfulButtons : OdinEditorWindow
+    {
+        [MenuItem("Tools/Helpful Buttons")]
+        public static void OpenWindow()
+        {
+            GetWindow<HelpfulButtons>().Show();
+        }
+        
+        [Button, BoxGroup("Lobby")]
+        private void LocalhostLobby()
+        {
+            LoadScene("Assets/HotPotato/Scenes/Menus/Lobby/LocalhostLobby.unity");
+        }
+        
+        [Button, BoxGroup("Lobby")]
+        private void SteamLobby()
+        {
+            LoadScene("Assets/HotPotato/Scenes/Menus/Lobby/SteamLobby.unity");
+        }
+        
+        [Button, BoxGroup("Game")]
+        private void MainGame()
+        {
+            LoadScene("Assets/HotPotato/Scenes/MainGame/MainGame.unity");
+        }
+        
+        private void LoadScene(string scenePath)
+        {
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene(scenePath);
+            }
+        }
+    }
+}
