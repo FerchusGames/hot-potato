b70303d128a8bf4abd07583e5badbc110b3654ee
Ferchus <ferchusgames@protonmail.com>
2025-03-31 18:47:11 -0600

    Utility for faster localhost match setup

---
+++ Assets/HotPotato/Scripts/Network/LocalhostLobbySetup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FishNet.Object;
+using FishNet.Transporting.Tugboat;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HotPotato.Network
+{
+    public class LocalhostLobbySetup : MonoBehaviour
+    {
+        [Required]
+        [SerializeField] private Tugboat _tugboat;
+        
+        private void Awake()
+        {
+            if (ParrelSync.ClonesManager.IsClone())
+            {
+                ConnectAsClient();
+                return;
+            }
+            ConnectAsHost();
+        }
+        
+        private void ConnectAsClient()
+        {
+            _tugboat.StartConnection(false);
+        }
+
+        private void ConnectAsHost()
+        {
+            _tugboat.StartConnection(true);
+            _tugboat.StartConnection(false);
+        }
+    }
+}
