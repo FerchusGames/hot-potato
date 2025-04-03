ea864c78d25d813e63fd7a595057a1762d868022
Ferchus <ferchusgames@protonmail.com>
2025-04-03 11:49:09 -0600

    Player possesses camera when connecting

---
+++ Assets/HotPotato/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,33 @@
+﻿using FishNet.Object;
+using Sirenix.OdinInspector;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace HotPotato.Player
+{
+    public class PlayerCameraController : NetworkBehaviour
+    {
+        [Required]
+        [SerializeField] private CinemachineCamera _onTurnCamera;
+        
+        [Required]
+        [SerializeField] private CinemachineCamera _notOnTurnCamera;
+
+        public override void OnStartClient()
+        {
+            if (!IsOwner)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            
+            SetCameraAsLive(_onTurnCamera);
+        }
+
+        private static void SetCameraAsLive(CinemachineCamera camera)
+        {
+            camera.enabled = false;
+            camera.enabled = true;
+        }
+    }
+}
