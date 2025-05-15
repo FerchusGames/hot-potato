b9d94f790260151e194aaf29f6d8a3c25826d493
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2025-05-15 12:06:30 -0600

    Bomb explosion implementation (#145)
    Closes #135 
    
    When the bomb explodes, explosion particle effects appear.
    
    https://github.com/user-attachments/assets/9b89ede8-24e9-41f1-b02c-be7c45efa0f0

---
+++ Assets/HotPotato/Scripts/Bomb/BombExploder.cs
@@ -0,0 +1,38 @@
+﻿using FishNet.Object;
+using HotPotato.GameFlow.TurnStateMachine;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HotPotato.Bomb
+{
+    public class BombExploder : NetworkBehaviour
+    {
+        EventBinding<ModuleExplodedExitStateEvent> _explodedEventBinding;
+        
+        [Required]
+        [SerializeField] private ParticleSystem _explodeParticles;
+
+        public override void OnStartServer()
+        {
+            _explodedEventBinding = new EventBinding<ModuleExplodedExitStateEvent>(Explode);
+            EventBus<ModuleExplodedExitStateEvent>.Register(_explodedEventBinding);
+        }
+
+        public override void OnStopServer()
+        {
+            EventBus<ModuleExplodedExitStateEvent>.Deregister(_explodedEventBinding);
+        }
+
+        [Server]
+        private void Explode()
+        {
+            ExplodeClientRpc();
+        }
+        
+        [ObserversRpc]
+        private void ExplodeClientRpc()
+        {
+            _explodeParticles.Play();
+        }
+    }
+}
