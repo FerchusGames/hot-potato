674259cc65b1f1b48cd2b3d14b897a0711571a9e
Ferchus <ferchusgames@protonmail.com>
2025-05-29 19:27:11 -0600

    PauseMenu implemented

---
+++ Assets/HotPotato/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HotPotato.UI
+{
+    public class PauseMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _pauseMenu;
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                TogglePauseMenu();
+            }
+        }
+
+        public void TogglePauseMenu()
+        {
+            _pauseMenu.SetActive(!_pauseMenu.activeSelf);
+        }
+
+        public void QuitMatch()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
