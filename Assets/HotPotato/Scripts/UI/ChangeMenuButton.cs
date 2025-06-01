423a05ff8e4344dee0b1c3c71413fc7c994747c9
Ferchus <ferchusgames@protonmail.com>
2025-05-31 19:07:28 -0600

    Hover and click SFX

--- Assets/HotPotato/Scripts/UI/ChangeMenuButton.cs
+++ Assets/HotPotato/Scripts/UI/ChangeMenuButton.cs
@@ -1,14 +1,19 @@
-﻿using Sirenix.OdinInspector;
+﻿using HotPotato.Audio;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace HotPotato.UI
 {
     [RequireComponent(typeof(Button))]
-    public class ChangeMenuButton : MonoBehaviour
+    public class ChangeMenuButton : MonoBehaviour, ISelectHandler, IPointerEnterHandler
     {
         [SerializeField, Required] private GameObject _currentMenu;
         [SerializeField, Required] private GameObject _targetMenu;
+        
+        [SerializeField] private EventReferenceSO _hoverSoundEventReference;
+        [SerializeField] private EventReferenceSO _clickSoundEventReference;
         
         private Button _button;
         
@@ -20,8 +25,35 @@ namespace HotPotato.UI
         
         private void ChangeMenu()
         {
+            PlayClickSound();
             _targetMenu.SetActive(true);
             _currentMenu.SetActive(false);
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            PlaySelectedSound();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PlaySelectedSound();
+        }
+        
+        private void PlayClickSound()
+        {
+            if (_clickSoundEventReference != null)
+            {
+                AudioManager.Instance.PlayOneShot(_clickSoundEventReference.EventReference, transform.position);
+            }
+        }
+        
+        private void PlaySelectedSound()
+        {
+            if (_hoverSoundEventReference != null)
+            {
+                AudioManager.Instance.PlayOneShot(_hoverSoundEventReference.EventReference, transform.position);
+            }
         }
     }
 }
