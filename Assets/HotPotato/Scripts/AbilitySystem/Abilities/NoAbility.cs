53e1c89725154a5f10adb717e33d30ed74078b0f
Ferchus <ferchusgames@protonmail.com>
2025-06-02 14:57:02 -0600

    Changed "No ability played" text

--- Assets/HotPotato/Scripts/AbilitySystem/Abilities/NoAbility.cs
+++ Assets/HotPotato/Scripts/AbilitySystem/Abilities/NoAbility.cs
@@ -5,7 +5,7 @@ namespace HotPotato.AbilitySystem.Abilities
     public class NoAbility : IAbility
     {
         public AbilityType AbilityType { get; private set; } = AbilityType.NoAbility;
-        public string Message { get; } = "No ability played";
+        public string Message { get; } = "Time is ticking";
 
         public async UniTaskVoid Execute()
         {
@@ -14,7 +14,7 @@ namespace HotPotato.AbilitySystem.Abilities
                 Ability = this,
             });
             
-            await UniTask.Delay(2000);
+            await UniTask.Delay(1000);
             EventBus<AbilityFinishedEvent>.Raise(new AbilityFinishedEvent());
         }
     }
