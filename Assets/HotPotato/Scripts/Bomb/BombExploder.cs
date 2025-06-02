cb48e4307e4c7d8a72e90043ddef6837a72da22f
Ferchus <ferchusgames@protonmail.com>
2025-06-02 17:33:55 -0600

    Defused sound effect

--- Assets/HotPotato/Scripts/Bomb/BombExploder.cs
+++ Assets/HotPotato/Scripts/Bomb/BombExploder.cs
@@ -9,22 +9,27 @@ namespace HotPotato.Bomb
     public class BombExploder : NetworkBehaviour
     {
         private EventBinding<ModuleExplodedExitStateEvent> _explodedEventBinding;
-        
-        [Required]
-        [SerializeField] private ParticleSystem _explodeParticles;
+        private EventBinding<ModuleDefusedExitStateEvent> _defusedEventBinding;
 
-        [Required]
-        [SerializeField] private EventReferenceSO _explodeSoundEventReference;
-        
+        [Required] [SerializeField] private ParticleSystem _explodeParticles;
+
+        [Required] [SerializeField] private EventReferenceSO _explodeSoundEventReference;
+
+        [Required] [SerializeField] private EventReferenceSO _defuseSoundEventReference;
+
         public override void OnStartServer()
         {
             _explodedEventBinding = new EventBinding<ModuleExplodedExitStateEvent>(Explode);
             EventBus<ModuleExplodedExitStateEvent>.Register(_explodedEventBinding);
+            
+            _defusedEventBinding = new EventBinding<ModuleDefusedExitStateEvent>(Defuse);
+            EventBus<ModuleDefusedExitStateEvent>.Register(_defusedEventBinding);
         }
 
         public override void OnStopServer()
         {
             EventBus<ModuleExplodedExitStateEvent>.Deregister(_explodedEventBinding);
+            EventBus<ModuleDefusedExitStateEvent>.Deregister(_defusedEventBinding);
         }
 
         [Server]
@@ -32,13 +37,25 @@ namespace HotPotato.Bomb
         {
             ExplodeClientRpc();
         }
-        
+
         [ObserversRpc]
         private void ExplodeClientRpc()
         {
             EventBus<BombExplodedClientEvent>.Raise(new BombExplodedClientEvent());
             _explodeParticles.Play();
             AudioManager.Instance.PlayOneShot(_explodeSoundEventReference.EventReference, transform.position);
+        }
+
+        [Server]
+        public void Defuse()
+        {
+            DefuseClientRpc();
+        }
+
+        [ObserversRpc]
+        private void DefuseClientRpc()
+        {
+            AudioManager.Instance.PlayOneShot(_defuseSoundEventReference.EventReference, transform.position);
         }
     }
 }
